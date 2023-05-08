0077427d82053cf45ccb1a1a33dff38f4b02b716
JinhwanB <paewhal20@gmail.com>
2023-05-08 13:20:04 +0900

    2023-04-24

--- src/j2023_04_24/Calculator.java
+++ src/j2023_04_24/Calculator.java
@@ -1,7 +1,7 @@
 package j2023_04_24;
 
-public abstract class Calculator {
-	public abstract int add(int a, int b);
-	public abstract int subtract(int a, int b);
-	public abstract double average(int[] a);
+public abstract class Calculator {// 추상 클래스
+	public abstract int add(int a, int b);// 추상 메소드
+	public abstract int subtract(int a, int b);// 추상 메소드
+	public abstract double average(int[] a);// 추상 메소드
 }
